05cb2bde3a14dd8f7c62054e58d8f8b4ffa954d9
Eratic <69852567+3ratic@users.noreply.github.com>
2022-08-28 22:39:42 +0100

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -1,6 +1,6 @@
 @echo off
 title Compiling DisFunc
 echo Starting to compile DisFunc
-pyinstaller DisFunc.py --uac-admin --onefile --noconsole
+pyinstaller source.py --uac-admin --onefile --noconsole
 echo. 
 echo Finished compiling DisFunc
